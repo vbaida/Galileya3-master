76c5aba2735b06770b2231fc22e7786cc5c67195
Volodymyr <92519042+vbaida@users.noreply.github.com>
2022-02-19 15:56:26 +0200

    js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 const form = document.forms["form"];
+const button = form.elements["button"];
+const inputArr = Array.from(form);
+const validInputArr = [];
 
-const button = form.element["button"];
+inputArr.forEach((el) =>{
+    if(el.hasAttribute("data-reg")) {
+        el.setAttribute("is-valid", "0")
+        validInputArr.push(el);
+    }
+});
 
+console.log(validInputArr);
 form.addEventListener("input", inputHandler);
+button.addEventListener("click", buttonHandler);
 
 function inputHandler ({target}) {
     if (target.hasAttribute("data-reg")) {
@@ -15,8 +25,25 @@ function inputCheck (el) {
     const reg = new RegExp(inputReg);
     if (reg.test (inputValue)) {
         el.style.border = "2px solid rgb(0, 196, 0)";
+        el.setAttribute("is-valid", "1")
+
     }
     else {
         el.style.border = "2px solid rgb(255,0,0)"
+        el.setAttribute("is-valid", "0")
+
+    }
+}
+
+function buttonHandler (e) {
+    const isAllValid = [];
+    validInputArr.forEach((el)=> {
+        isAllValid.push(el.getAttribute("is-valid"));
+    });
+    const isValid = isAllValid.reduce((acc, current) => {
+        return acc && current
+    });
+    if (!Boolean(Number(isValid))) {
+        e.preventDefault();
     }
 }
